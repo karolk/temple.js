eeb18751a2412767d2607556ebe1c2697b6fd72a
Karol Kowalski <kowcik@gmail.com>
2012-03-28 17:18:42 -0300

    small rewrite, added support for raw html and templating attributes, plus templeLeaf for leaf node templating

--- temple.js
+++ temple.js
@@ -1,36 +1,68 @@
+(function($) {
+
+$.fn.templeLeaf = function(propName, propValue) {
+        
+    if ( (typeof propValue).match(/string|number/) ) {
+        this
+            .filter('[data-templ="'+propName+'"]')
+            .text(propValue);
+        this
+            .filter('[data-templ-html="'+propName+'"]')
+            .html(propValue);
+    }
+    else {
+        if ('attr' in propValue) {
+            this
+                .filter('[data-templ="'+propName+'"]')
+                .attr(propValue.attr, propValue.value)
+        }
+    }
+    
+    return this;
+
+}
+
 $.fn.temple = function(stash) {
     var rootNode = this,
-        childNodes = rootNode.find('[data-templ]'),
+        dataSelector = '[data-templ], [data-templ-html]',
+        dataNodes = rootNode
+            .filter(dataSelector)
+            .add( rootNode.find(dataSelector) ),
         stencil;
-
+            
     for (var propName in stash) {
     
-        if ( $.isArray(stash[propName]) ) {
-            var stashArray = stash[propName];
-            var $node = childNodes.filter('[data-templ="'+propName+'"]'),
-                isCollectionNode = $node.is('ol, ul, dl, table');
-            if ($node.length && isCollectionNode) {
+        var stashProp = stash[propName];
+        
+        if ( $.isArray(stashProp) ) {
+        
+            var $node = dataNodes.filter([
+                    '[data-templ="'+propName+'"]',
+                    '[data-templ-html="'+propName+'"]'
+                    ].join(','));
+                    
+            if ($node.length) {
                 stencil = $node.children().eq(0);
                 if (stencil.length) {
                     stencil = stencil.clone();
                     $node.empty();
+                    $.each(stashProp, function(index, stashArrayElem) {
+                        var $subNode = stencil.clone();
+                        $subNode.temple(stashArrayElem)
+                        $node.append($subNode);
+                    });
                 }
-                stashArray.forEach(function(stashArrayElem) {
-                    var $subNode = stencil.clone();
-                    $subNode.temple(stashArrayElem)
-                    $node.append($subNode);
-                })
-            }
+            }   
         }
         
         else {
-            childNodes
-                .filter('[data-templ="'+propName+'"]')
-                .text(stash[propName])
+            
+            dataNodes.templeLeaf(propName, stashProp);
+            
         }
-        
     }
     
     return rootNode;
-    
-};
+
+}
+})($);
